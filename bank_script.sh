8194e82ee478caf18f1e43c6512fc963aaa6ef3b
Carlos Mondo <48037173+carlosmondo@users.noreply.github.com>
2019-05-04 04:07:29 -0300

    Update bank_script.sh

--- bank_script.sh
+++ bank_script.sh
@@ -1,45 +1,45 @@
 #!/usr/bash
 
-echo "Banco Shell Script"
+echo "\nBanco Shell Script"
 echo -n "Informe seu usuário: "
 read user
 
-echo "Buscando informações..."
+echo "\nBuscando informações..."
 wget -q https://raw.githubusercontent.com/leandersonandre/shell-script/master/lista_usuarios.txt
-
-result=`cat lista_usuarios.txt | grep $user`
+result=$(cat lista_usuarios.txt | grep -w "$user")
 
 if [ $result ]; then
-  echo "Usuário $user encontrado."
-  echo "Baixando dados do usuário..."
-  wget -q https://github.com/leandersonandre/shell-script/blob/master/$user.tar.gz?raw=true
-  echo "Download completo"
-  mv $user.tar.gz?raw=true $user.tar.gz
-  gzip -d $user.tar.gz
-  tar -xvf $user.tar
-
-  option=0
-  while [ $option -ne 3 ]; do
-    echo -n "Selecione uma opção: \n1. Ver Saldo \n2. Ver Extrato \n3. Sair \nDigite sua opção:"
-    read option
-
-    if [ $option -eq 1 ]; then
-      cat saldo.txt
-    elif [ $option -eq 2 ]; then
-      cat extrato.txt
-    elif [ $option -eq 3 ]; then
-      echo "Excluindo arquivos..."
-      rm saldo.txt
-      rm extrato.txt
-      rm lista_usuarios.txt
-      rm -r $user
-      
-      echo "Obg, volte sempre"
-      echo "Bank Shell"
-    else 
-      echo "Opção inválida."
-    fi 
-  done
+    echo "\nUsuário encontrado."
+    echo "Baixando dados do usuário..."
+    wget -q https://github.com/leandersonandre/shell-script/blob/master/$user.tar.gz?raw=true
+    
+    mv $user.tar.gz?raw=true $user.tar.gz
+    tar -xf $user.tar.gz
+    echo "Download completo"
+    cd $user
+    option=0
+    
+    while [ $option -ne 3 ]; do
+        echo -n "\n\nSelecione uma opção:\n1. Ver Saldo\n2. Ver Extrato\n3. Sair\nDigite sua opção: "
+        read option
+        echo
+        
+        if [ $option -eq 1 ]; then
+            cat saldo.txt
+        elif [ $option -eq 2 ]; then
+            cat extrato.txt
+        elif [ $option -eq 3 ]; then
+            echo "Excluindo arquivos..."
+            cd ..
+            rm -r lista_usuarios.txt $user.tar.gz $user
+        else
+            echo "Opção invalida\nObg, volte semppre\nBank Shell"
+        fi
+    done
 else
-  echo "Usuário $user não encontrado."
+    echo "O usuário $user não encontrado\n"
+    
+    if [ lista_usuarios.txt ]; then
+        rm lista_usuarios.txt
+    fi
 fi
